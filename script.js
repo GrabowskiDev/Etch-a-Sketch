b4ed3bec7cb8a81c24dbc905e655bad0069af86f
GrabowskiDev <lukaszgra2004@gmail.com>
2021-04-18 12:24:06 +0200

    Layout and pseudocode

---
+++ script.js
@@ -0,0 +1,20 @@
+//On start of page, set grid var to 16
+
+//Make a container with 16x16 squares (divs)(grid)  
+    //The script will add a choosen number of divs to
+    //parent div in for loop 
+    //Add css rule to parent div with grid-template-columns: repeat()
+    ////we will also have to calculate the size of each
+    ////div based on size of parent div and number of
+    ////children divs
+
+//Change the color to random variation of color on hover
+    //colors in rgb format, if not, they will be converted
+    //user will pick a color, whether in hex or rgb
+    ////user will pick a color by typing it in a box.
+    //random mesh pattern will be created by changing
+    //each value by random number in range from x to y
+    
+//Button that will make new grid of squares (AxA)
+    //Prompt will ask for a value
+    //This value will then be send to making container function
